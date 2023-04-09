583f8b7685233c59d7ced06e96a3b7b0d5945c95
Iryna Oleksyshyn <irynkaoleksyshyn@gmail.com>
2023-04-09 19:01:30 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+let timerId = null;
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', onChangeColor);
+stopBtn.addEventListener('click', onStopChangeColor)
+
+function onChangeColor() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    timerId = setInterval(() => {
+        const bodyEl = document.querySelector('body');
+        bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function onStopChangeColor() {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
