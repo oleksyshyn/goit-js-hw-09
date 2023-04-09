f4a9ab0e14e9614103253cb68606da045c3edb92
oleksyshyn <117373813+oleksyshyn@users.noreply.github.com>
2023-04-09 16:02:02 +0000

    Deploying to gh-pages from @ oleksyshyn/goit-js-hw-09@583f8b7685233c59d7ced06e96a3b7b0d5945c95 🚀

---
+++ 01-color-switcher.a7042a10.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval((function(){document.querySelector("body").style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.a7042a10.js.map
