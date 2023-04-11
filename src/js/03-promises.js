0fec62a4be2f3d1fc16365a96b42bed5ba16dd6a
Iryna Oleksyshyn <irynkaoleksyshyn@gmail.com>
2023-04-11 22:10:05 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  let { delay, step, amount } = event.target.elements;
+  let position = 1;
+  delay = Number(delay.value);
+  step = Number(step.value);
+  amount = Number(amount.value);
+
+  for (let i = 0; i < amount; i += 1) {
+
+    createPromise(position, delay)
+    .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
+    position += 1;
+    delay += step;
+  };
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });  
+};
